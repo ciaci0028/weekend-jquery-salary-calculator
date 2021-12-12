e131de6a2c1948a7fb4011b350bfff71b0b53307
Sabrina Ciaciura <ciaci0028@gmail.com>
2021-12-11 18:56:46 -0600

    added form to html

--- app.js
+++ app.js
@@ -3,11 +3,19 @@
 $(document).ready(onReady);
 
 function onReady() {
+    // Submit new information upon click
     $('#submitButton').on('click', submitInfo);
 }
 
 function submitInfo () {
-    console.log('hi');
-    // let firstName = $()
 
+    // Creating variables for the functions in the input fields
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let identification = $('#identification').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
+
+    // Now I must take the input variables 
+    
 }
